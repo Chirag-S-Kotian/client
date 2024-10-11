393cac52dcb79d6eeb189ae574932eae0e1ecd4c
Chirag-S-kotian <ckotian117@gmail.com>
2024-10-11 11:12:59 +0530

    "Update API_URL from hardcoded 'http://localhost:4000/api' to environment variable NEXT_PUBLIC_API_URL"

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from 'axios';
 
-const API_URL = 'http://localhost:4000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
