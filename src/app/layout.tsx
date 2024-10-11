278f97ecfb2f0d8d00540d9994fa494498ca156a
Chirag-S-kotian <ckotian117@gmail.com>
2024-10-11 22:50:46 +0530

    "Added @vercel/analytics dependency and imported Analytics component in layout.tsx"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@/components/ui/toaster'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={`${inter.className} overflow-x-hidden transition-colors duration-500`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
+          <Analytics />
           <Toaster />
         </ThemeProvider>
       </body>
