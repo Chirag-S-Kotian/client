19efb406c5b6715a04b241a0ea59f45b31af0a5e
Chirag-S-kotian <ckotian117@gmail.com>
2024-10-21 22:35:07 +0530

    "Update ThemeProvider: change 'disableSystem' to 'enableSystem={false}' and add comments"

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -7,8 +7,8 @@ import { type ThemeProviderProps } from "next-themes/dist/types"
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider
-      defaultTheme="dark"
-      disableSystem 
+      defaultTheme="dark" // Set the default theme to dark
+      enableSystem={false} // Disable the system theme option
       {...props}
     >
       {children}
